ed82683c393f51c027e48bb64f040258a5304249
Matthieu Daulhiac <matthieu@blanknetwork.com>
2023-05-26 13:15:41 +0800

    add button for resume and illustration

--- src/components/pages/home/about-me-section.tsx
+++ src/components/pages/home/about-me-section.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import { poppins } from '../../base/fonts';
 import { useMemo } from 'react';
+import Image from 'next/image';
 
 const CONTENT = [
   {
@@ -8,18 +9,21 @@ const CONTENT = [
     title: 'About Me',
     content:
       'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+    button: 'My Resume',
   },
   {
     locale: 'fr',
     title: 'Qui suis-je?',
     content:
       'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+    button: 'Mon CV',
   },
   {
     locale: 'cn',
     title: '关于我',
     content:
       'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+    button: ' 我的简历',
   },
 ];
 
@@ -33,12 +37,26 @@ const AboutMeSection = () => {
 
   return (
     <section className="max-w-6xl mx-auto px-4 py-10 flex flex-col">
-      <h2
-        className={`uppercase text-4xl md:text-7xl ${poppins.className} font-semibold`}
-      >
-        {content?.title}
-      </h2>
-      <p className="my-10">{content?.content}</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-0">
+        <div className="flex flex-col justify-around">
+          <h2
+            className={`uppercase text-4xl md:text-7xl ${poppins.className} font-semibold`}
+          >
+            {content?.title}
+          </h2>
+
+          <p className="my-10">{content?.content}</p>
+          <button>{content?.button}</button>
+        </div>
+        <div className="relative h-full w-full min-h-[500px]">
+          <Image
+            alt=""
+            src="/about-me-illustration.png"
+            fill
+            style={{ objectFit: 'contain', objectPosition: 'center' }}
+          />
+        </div>
+      </div>
     </section>
   );
 };
